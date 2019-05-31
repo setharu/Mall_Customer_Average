bfb79ff7bfb5b159421e4b4178946c9a3f62c60f
setharu <arushiseth2011@gmail.com>
2019-05-31 23:35:23 +0530

    Add files via upload

---
+++ Mall_Customer_average_dict.py
@@ -0,0 +1,25 @@
+'''Find out the average of Male and Female Spending Score Using List'''
+
+file_path=r'D:/Mall_Customers_Excercise/machineLearningAZ-master/machineLearningAZ-master/Machine Learning A-Z Template Folder/Part 4 - Clustering/Section 24 - K-Means Clustering/Mall_Customers.csv'
+
+infile = open(file_path,'r')
+gender_dict = dict()
+
+for line in infile:
+    columns = line.split(',')
+    gender, score = columns[1], columns[4]
+    if line[0].isalpha():
+        continue
+    if gender in gender_dict:
+        gender_dict[gender].append(int(score))
+    else:
+        gender_dict[gender]=list()
+
+print(gender_dict)
+
+for gender, score in gender_dict.items():
+    print(gender, sum(score)/len(score))
+
+infile.close()
+
+print("End of the Program")
